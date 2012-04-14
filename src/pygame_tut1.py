7b2d25c7da51ca6043d42ce598726963ea066ff3
Eric Gerlach <eric@gerlach.ca>
2012-04-14 18:26:18 -0400

    Refactor UpDownBox to have a MovingBox in the middle.  This will make it easier to have a PlayerBox

--- src/pygame_tut1.py
+++ src/pygame_tut1.py
@@ -7,12 +7,11 @@ Created on 2012-04-14
 import pygame
 
 class Box(pygame.sprite.Sprite):
-    def __init__(self, colour, initial_position, speed):
+    def __init__(self, colour, initial_position):
         """Builds a Box sprite
         
         @param colour - a list of RGB values for the colour of the box
         @param initial_position - a list of two values for the top left coordinate of the box
-        @param speed - number of pixels per second that the box should move
         """
         # Call parent contstructor
         pygame.sprite.Sprite.__init__(self)
@@ -24,26 +23,63 @@ class Box(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = initial_position
         
+class MovingBox(Box):
+    def __init__(self, colour, initial_position, speed):
+        """Builds a Box sprite that moves up and down on its own
+        
+        @param colour - a list of RGB values for the colour of the box
+        @param initial_position - a list of two values for the top left coordinate of the box
+        @param speed - number of pixels per second that the box should move
+        """
+        Box.__init__(self, colour, initial_position)
+        
         self.going_down = True
          
         self.speed = 1000/speed
         self.next_update_time = 0
         
-    def update(self, current_time, bottom):
+    def update(self, current_time):
         if self.next_update_time < current_time:
-            if self.rect.bottom >= bottom:
-                self.going_down = False
-            elif self.rect.top <= 0:
-                self.going_down = True
-                
-            self.rect.top += 1 if self.going_down else -1
-            
+            self.move()
             self.next_update_time += self.speed
+            
+    def move(self):
+        pass
+        
+class UpDownBox(MovingBox):
+    def __init__(self, colour, initial_position, speed, bottom):
+        """Builds a Box sprite that moves up and down on its own
+        
+        @param colour - a list of RGB values for the colour of the box
+        @param initial_position - a list of two values for the top left coordinate of the box
+        @param speed - number of pixels per second that the box should move
+        """
+        MovingBox.__init__(self, colour, initial_position, speed)
+        
+        self.going_down = True
+        self.bottom = bottom
+         
+    def move(self):    
+        if self.rect.bottom >= self.bottom:
+            self.going_down = False
+        elif self.rect.top <= 0:
+            self.going_down = True
+            
+        self.rect.top += 1 if self.going_down else -1
+        
+            
+class PlayerBox(UpDownBox):
+    def __init__(self, colour, initial_position, speed):
+        """Builds a Box sprite that responds to keyboard commands of the player
+        
+        @param colour - a list of RGB values for the colour of the box
+        @param initial_position - a list of two values for the top left coordinate of the box
+        @param speed - number of pixels per second that the box should move while a key is held down
+        """
+        pass
         
         
-
-if __name__ == '__main__':
-    import sys
+def mainloop():        
     pygame.init()
     
     size = width, height = 320, 240
@@ -57,18 +93,21 @@ if __name__ == '__main__':
     
     boxes = pygame.sprite.RenderUpdates()
     
-    for b in [Box([255,0,0], [0,0], 100),
-             Box([0,255,0], [60, 137], 25),
-             Box([0,0,255], [180, 252], 300)]:
+    for b in [UpDownBox([255,0,0], [0,0], 100, 240),
+             UpDownBox([0,255,0], [60, 137], 25, 240),
+             UpDownBox([0,0,255], [180, 252], 300, 240)]:
         boxes.add(b)
         
     while 1:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-            if event.type == pygame.KEYDOWN: sys.exit()
+            if event.type == pygame.QUIT: return
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE: return
             
+        boxes.update(pygame.time.get_ticks())
         boxes.clear(screen, background)
-        boxes.update(pygame.time.get_ticks(), 240)
         boxes.draw(screen)
             
         pygame.display.flip()
+
+if __name__ == '__main__':
+    mainloop()
